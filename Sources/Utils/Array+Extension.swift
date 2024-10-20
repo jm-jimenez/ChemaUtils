dd41ec9df2bc5ac57ed73acc38800dc034b0aa26
José María Jiménez <josemariajimenezperez@gmail.com>
2024-10-20 20:08:50 +0200

    UPDATE: Array+Extension

---
+++ Sources/Utils/Array+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Array+Extension.swift
+//  ChemaUtils
+//
+//  Created by José María Jiménez on 20/10/24.
+//
+
+public extension Array {
+    var isNotEmpty: Bool {
+        return count > 0
+    }
+}
