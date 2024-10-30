28e3c33996d6aceaa45b530a9fe26cc93d2f8ec8
José María Jiménez <josemariajimenezperez@gmail.com>
2024-10-30 19:22:01 +0100

    UPDATE Use Case conforming to InjectableCapable

--- Sources/Utils/UseCase.swift
+++ Sources/Utils/UseCase.swift
@@ -5,7 +5,9 @@
 //  Created by José María Jiménez on 20/10/24.
 //
 
-public protocol UseCase {
+import DependencyResolver
+
+public protocol UseCase: InjectableCapable {
     associatedtype Success
     func execute() async throws -> Success
 }
